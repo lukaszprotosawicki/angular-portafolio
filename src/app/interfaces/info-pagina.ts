090475afa28b443940f194ae7cc461bd21fd88f1
lukasz protosawicki <lukas.es.p@o2.pl>
2021-08-03 22:21:53 +0200

    add json + service

---
+++ src/app/interfaces/info-pagina.ts
@@ -0,0 +1,13 @@
+
+
+
+export interface InfoPagina  {
+  titulo?: string;
+  email?: string;
+  pagina_autor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagran?: string;
+  tublr?: string;
+  equipo_trabajo?: any[];
+}
