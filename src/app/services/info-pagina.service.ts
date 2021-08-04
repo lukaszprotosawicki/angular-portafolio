756e0cebb8ac3c41a4c89f4ebaea52f7de453787
lukasz protosawicki <lukas.es.p@o2.pl>
2021-08-04 16:06:06 +0200

    add create firebase and load info in web

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,11 +10,26 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = []
+
   constructor(private http: HttpClient) {
-    console.log("servicio")
+
+    this.cargarInfo()
+    this.cargarEquipo()
+   }
+
+   private cargarInfo() {
     this.http.get('assets/data/data-pagina.json').subscribe(( resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
+
+    })
+   }
+
+   private cargarEquipo() {
+    this.http.get('https://angular-html-c039d-default-rtdb.europe-west1.firebasedatabase.app/equipo.json').subscribe(( resp: any) => {
+
+      this.equipo = resp;
 
       console.log(resp)
     })
